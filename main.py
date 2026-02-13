b58fb3d663b1651ae32ba7e91798fa4603a92f84
Dominick Cheers <cheersdominick@gmail.com>
2026-02-13 03:50:55 -0500

    Fixed CORS placement

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For now (we'll lock this later)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
